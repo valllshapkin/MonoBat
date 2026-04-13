b2902879cf81359663d73e21902270fe3553d971
Valeriy Shapkin <valllshapkin@gmail.com>
2026-04-13 23:47:45 +0300

    1

---
+++ Scripts/init.sh
@@ -0,0 +1,5 @@
+#repo init -u git@github.com:valllshapkin/MonoBat.git -m manifests/default.xml
+repo init -u https://github.com/valllshapkin/MonoBat.git \
+          -m Environment/Manifest/default.xml \
+          -b main
+
